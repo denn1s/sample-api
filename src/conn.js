410f8a243bbbce709d0dd8da1399f64584f78d05
Dennis Aldana <dennis.aldana@gmail.com>
2024-03-05 20:23:15 -0600

    chore: change user and password for database

--- src/conn.js
+++ src/conn.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'blog_user',
-    database: 'blog_db',
-    password: 'blog_password',
+    user: 'dennis',
+    database: 'blog_dennis',
+    password: 'dennis',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
