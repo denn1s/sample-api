378737151d665d114663a49c799c67af1ac42914
Dennis Aldana <dennis.aldana@gmail.com>
2024-03-05 20:31:15 -0600

    chore: pm2 changes

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [{
+    name: 'blog dennis',
+    script: 'npm',
+    args: 'start',
+    log_file: './blog_dennis.txt',
+    merge_logs: true
+  }]
+}
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import express from 'express'
 
 const app = express()
 
-app.post('/hello', async (req, res) => {
-  res.send('HELLO FROM POST')
+app.get('/hello', async (req, res) => {
+  res.send('HELLO FROM MY SERVER')
 })
 
-const port = 5000
+const port = 22666
 
 app.listen(port, () => {
   console.log(`Server listening at http://127.0.0.1:${port}`)
